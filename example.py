b5a66e74a3c17230be01fd99aed422cb91a0ecf0
Andrei Valdez <andreivaldezau@gmail.com>
2024-05-19 16:08:37 +1000

    Add GET request for Sydney Trains

---
+++ example.py
@@ -0,0 +1,24 @@
+import requests
+import json
+import os
+from dotenv import load_dotenv
+
+import gtfs_realtime_1007_pb2
+
+# from google.protobuf.json_format import MessageToDict, MessageToJson
+
+load_dotenv()
+API_KEY = os.getenv("TFNSW_API_KEY")
+
+url = "https://api.transport.nsw.gov.au/v2/gtfs/vehiclepos/sydneytrains"
+headers = {
+    "accept": "application/x-google-protobuf",
+    "Authorization": f"apikey {API_KEY}",
+}
+
+feed = gtfs_realtime_1007_pb2.FeedMessage()
+response = requests.get(url, headers=headers)
+if response.status_code == 200:
+    feed.ParseFromString(response.content)
+    for entity in feed.entity:
+        print(entity.id)
